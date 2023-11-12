3ef90f5e11a1d53dc32d1b848ba3c406bc3fc672
Sergey Semyonov <evlx@mail.ru>
2023-11-12 22:02:41 +0300

    added a start menu to get the user's choice

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from src.classes.class_api import HeadHunterAPI
-from src.file_saver import JSONSaver
-from src.utils import get_hh_vacancies
+from src.utils import parser_run
+
+print("Программа поиска вакансий запущена.")
 
 
 def main():
-    keyword = input('Введите поисковой запрос: ')
+    user_choice = input("===================\n"
+                        "Что вы хотите сделать:\n"
+                        "1 - Запустить поиск вакансий на порталах HH и SJ\n"
+                        "2 - Просмотреть вакансии загруженные в JSON файл\n"
+                        "3 - Выйти из программы\n"
+                        "Ваш выбор: ")
 
-    hh_api = HeadHunterAPI()
-
-    vacancies_hh = get_hh_vacancies(hh_api.get_vacancies(keyword))
-
-    json_saver = JSONSaver()
-    json_saver.add(vacancies_hh)
+    if user_choice == "1":
+        parser_run()
+        main()
+    elif user_choice == "2":
+        print("Мы работаем над этим")
+    elif user_choice == "3":
+        print("До свидания.")
+    else:
+        print("Вы ввели недопустимое значение. Попробуйте выбрать снова.")
+        main()
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+from src.classes.class_api import HeadHunterAPI
+from src.file_saver import JSONSaver
+
+
 def get_hh_vacancies(vacancy: list[dict]) -> list[dict]:
     """Форматирование объектов hh.ru"""
     vacancy_object = [{
@@ -7,3 +11,17 @@ def get_hh_vacancies(vacancy: list[dict]) -> list[dict]:
         'requirements': item['snippet']['requirement']
     } for item in vacancy]
     return vacancy_object
+
+
+def parser_run():
+    keyword = input('Введите поисковой запрос: ')
+    hh_api = HeadHunterAPI()
+
+    vacancies_hh = get_hh_vacancies(hh_api.get_vacancies(keyword))
+
+    json_saver = JSONSaver()
+    json_saver.add(vacancies_hh)
+
+    print(f"Поиск вакансий по запросу '{keyword}' завершен.\n"
+          f"Найдено {None} вакансий\n"
+          f"Данные успешно загружены в vacancy.json")
