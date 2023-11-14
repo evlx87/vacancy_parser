45253456872b2dd5aa3c92efc774e1b59c2252a7
Sergey Semyonov <evlx@mail.ru>
2023-11-14 20:12:43 +0300

    add unique_value_counter()

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import uuid
+
 from src.classes.class_api import HeadHunterAPI
 from src.file_saver import JSONSaver
 
@@ -5,6 +7,7 @@ from src.file_saver import JSONSaver
 def get_hh_vacancies(vacancy: list[dict]) -> list[dict]:
     """Форматирование объектов hh.ru"""
     vacancy_object = [{
+        'id': str(uuid.uuid4()),
         'title': item['name'],
         'link': item['alternate_url'],
         'salary': item['salary']['from'] if item.get('salary') else '0',
@@ -13,15 +16,23 @@ def get_hh_vacancies(vacancy: list[dict]) -> list[dict]:
     return vacancy_object
 
 
+def unique_value_counter(data):
+    unique_ids = set()
+    for d in data:
+        unique_ids.add(d['id'])
+    return unique_ids
+
+
 def parser_run():
     keyword = input('Введите поисковой запрос: ')
     hh_api = HeadHunterAPI()
 
     vacancies_hh = get_hh_vacancies(hh_api.get_vacancies(keyword))
 
+    uvc = unique_value_counter(vacancies_hh)
+
     json_saver = JSONSaver()
     json_saver.add(vacancies_hh)
 
     print(f"Поиск вакансий по запросу '{keyword}' завершен.\n"
-          f"Найдено {None} вакансий\n"
-          f"Данные успешно загружены в vacancy.json")
+          f"Добавлено {len(uvc)} вакансий в файл vacancy.json")
